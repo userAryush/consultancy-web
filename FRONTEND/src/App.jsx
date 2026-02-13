404ecf3385b7608735a93b4bcd1504338a7c6efd
userAryush <khatriaryush@gmail.com>
2026-02-13 21:48:09 +0545

    react and tailwind setup

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-blue-400">
+        React + Tailwind is Working 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
